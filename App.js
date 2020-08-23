6ccf1784d768d86396bb3d93f3012d6c97b69db0
piyush shrivastava <piyush.srivastavaimraw@gmail.com>
2020-08-23 21:35:28 +0530

    type(feat): fixing stream

--- App.js
+++ App.js
@@ -4,9 +4,24 @@ var request = require('request');
 
 http.createServer(onRequest).listen(3000);
 
+const { Transform } = require('stream');
+
+const transfomers = (url) => {
+  return new Transform({
+    transform(chunk, encoding, callback) {
+      this.push(chunk);
+      this.on('end', () => {
+        console.log({ chunk, url });
+        callback();
+      });
+    },
+  });
+};
+
 function onRequest(req, res) {
   var queryData = url.parse(req.url, true).query;
   if (queryData.url) {
+    const transformer = transfomers(queryData.url);
     request({
       url: queryData.url,
     })
